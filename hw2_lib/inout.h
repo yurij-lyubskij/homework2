a111c892ead988288d5d8b50ef41a28198680be0
yura <yura11011@yandex.com>
2022-04-02 02:44:09 +0300

    write test

--- hw2_lib/inout.h
+++ hw2_lib/inout.h
@@ -2,18 +2,17 @@
 #define HW2_STATIC_INOUT_H
 #include "utils.h"
 
-#define REPEATS 10
+#define REPEATS 2
 #define NAME "test"
 #define OUT "1.txt"
 
-void show_jobs(job_array* jobs);
 void show_list_by_job(results* result, FILE *fout);
-void print_to_file(FILE *fout, job_array *jobs, staff_array* staff);
-int read_from_file(FILE *fin, staff_array *staff, job_array* jobs);
 void show_buff(FILE *fout, staff_array*staff);
 int read_people(FILE *fin, staff_array *staff, uint16_t job_size);
 void write_random_people(staff_array* staff, FILE *fout);
 void write_jobs(FILE *fout);
+void print_jobs(FILE *fout, job_array *jobs);
+void print_people(FILE *fout, staff_array *staff);
 int read_jobs(FILE *fin, job_array *jobs);
 extern int rand_r (unsigned int *__seed);
 #endif //HW2_STATIC_INOUT_H
--- tests/test_db.cpp
+++ tests/test_db.cpp
@@ -4,7 +4,13 @@
 extern "C" {
 #include "utils.h"
 #include "inout.h"
+#ifdef REPEATS
+#undef REPEATS
+#define REPEATS 1
+#endif
 }
+
+
 
 TEST(db, default) {
     EXPECT_EQ(0, 0);
@@ -72,13 +78,76 @@ TEST(db, test_search) {
     staff.salary = salary;
     uint16_t job_index[] = {0, 1};
     staff.job_index = job_index;
-    double time_old = 0;
-    double time_young = 0;
     results young, old;
     find_young(&young, &staff, jobs.size);
     show_list_by_job(&young, fout);
     find_old(&old, &staff, jobs.size);
     show_list_by_job(&old, fout);
+    fclose(fout);
+    EXPECT_STRCASEEQ(output, etalon);
+    printf("");
+}
+
+
+TEST(db, test_write) {
+    char output[FILE_LEN];
+    char etalon[FILE_LEN] = "3\n"
+                            "продавец\n"
+                            "программист\n"
+                            "грузчик\n"
+                            "4\n"
+                            "костя\n"
+                            "антонов\n"
+                            "0\n"
+                            "23\n"
+                            "3\n"
+                            "30000.000000\n"
+                            "0\n"
+                            "анна\n"
+                            "беляева\n"
+                            "1\n"
+                            "25\n"
+                            "5\n"
+                            "500000.000000\n"
+                            "1\n"
+                            "костя\n"
+                            "антонов\n"
+                            "0\n"
+                            "23\n"
+                            "3\n"
+                            "30000.000000\n"
+                            "0\n"
+                            "анна\n"
+                            "беляева\n"
+                            "1\n"
+                            "25\n"
+                            "5\n"
+                            "500000.000000\n"
+                            "1\n";
+
+    FILE *fout = fmemopen(output, FILE_LEN, "w");
+    job_array jobs = {.job_names = NULL, 3, 3};
+    char* job_s[] = {"продавец", "программист", "грузчик"};
+    jobs.job_names = job_s;
+    staff_array staff;
+    staff.size = 2;
+    staff.capacity = 2;
+    char gender[] = {0, 1};
+    staff.gender = gender;
+    char* surname[] = {"антонов", "беляева"};
+    staff.surname = surname;
+    char* name[] = {"костя", "анна"};
+    staff.name = name;
+    uint8_t age[] = {23, 25};
+    staff.age = age;
+    uint8_t experience[] = {3, 5};
+    staff.experience = experience;
+    double salary[] = {30000, 500000};
+    staff.salary = salary;
+    uint16_t job_index[] = {0, 1};
+    staff.job_index = job_index;
+    print_jobs(fout, &jobs);
+    print_people(fout, &staff);
     fclose(fout);
     EXPECT_STRCASEEQ(output, etalon);
     printf("");
