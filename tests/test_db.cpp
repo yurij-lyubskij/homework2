feea4252104d6a1ca359897f31093a139b94370e
yura <yura11011@yandex.com>
2022-04-02 03:02:05 +0300

    full test

--- tests/test_db.cpp
+++ tests/test_db.cpp
@@ -84,8 +84,9 @@ TEST(db, test_search) {
     find_old(&old, &staff, jobs.size);
     show_list_by_job(&old, fout);
     fclose(fout);
+    free_result(&young);
+    free_result(&old);
     EXPECT_STRCASEEQ(output, etalon);
-    printf("");
 }
 
 
@@ -150,7 +151,137 @@ TEST(db, test_write) {
     print_people(fout, &staff);
     fclose(fout);
     EXPECT_STRCASEEQ(output, etalon);
-    printf("");
+}
+
+TEST(db, test_full) {
+    char output[FILE_LEN];
+    char input[FILE_LEN] = "3\n"
+                            "продавец\n"
+                            "программист\n"
+                            "грузчик\n"
+                            "4\n"
+                            "костя\n"
+                            "антонов\n"
+                            "0\n"
+                            "23\n"
+                            "3\n"
+                            "30000.000000\n"
+                            "0\n"
+                            "анна\n"
+                            "беляева\n"
+                            "1\n"
+                            "25\n"
+                            "5\n"
+                            "500000.000000\n"
+                            "1\n"
+                            "костя\n"
+                            "антонов\n"
+                            "0\n"
+                            "23\n"
+                            "3\n"
+                            "30000.000000\n"
+                            "0\n"
+                            "анна\n"
+                            "беляева\n"
+                            "1\n"
+                            "25\n"
+                            "5\n"
+                            "500000.000000\n"
+                            "1\n";
+    char etalon[FILE_LEN] = "person № 0\n"
+                            "name = костя\n"
+                            "surname = антонов\n"
+                            "gender = 0\n"
+                            "age = 23\n"
+                            "experience = 3\n"
+                            "salary = 30000.000000\n"
+                            "job_index = 0\n"
+                            "person № 1\n"
+                            "name = костя\n"
+                            "surname = антонов\n"
+                            "gender = 0\n"
+                            "age = 23\n"
+                            "experience = 3\n"
+                            "salary = 30000.000000\n"
+                            "job_index = 0\n"
+                            "\n"
+                            "person № 0\n"
+                            "name = анна\n"
+                            "surname = беляева\n"
+                            "gender = 1\n"
+                            "age = 25\n"
+                            "experience = 5\n"
+                            "salary = 500000.000000\n"
+                            "job_index = 1\n"
+                            "person № 1\n"
+                            "name = анна\n"
+                            "surname = беляева\n"
+                            "gender = 1\n"
+                            "age = 25\n"
+                            "experience = 5\n"
+                            "salary = 500000.000000\n"
+                            "job_index = 1\n"
+                            "\n"
+                            "\n"
+                            "person № 0\n"
+                            "name = костя\n"
+                            "surname = антонов\n"
+                            "gender = 0\n"
+                            "age = 23\n"
+                            "experience = 3\n"
+                            "salary = 30000.000000\n"
+                            "job_index = 0\n"
+                            "person № 1\n"
+                            "name = костя\n"
+                            "surname = антонов\n"
+                            "gender = 0\n"
+                            "age = 23\n"
+                            "experience = 3\n"
+                            "salary = 30000.000000\n"
+                            "job_index = 0\n"
+                            "\n"
+                            "person № 0\n"
+                            "name = анна\n"
+                            "surname = беляева\n"
+                            "gender = 1\n"
+                            "age = 25\n"
+                            "experience = 5\n"
+                            "salary = 500000.000000\n"
+                            "job_index = 1\n"
+                            "person № 1\n"
+                            "name = анна\n"
+                            "surname = беляева\n"
+                            "gender = 1\n"
+                            "age = 25\n"
+                            "experience = 5\n"
+                            "salary = 500000.000000\n"
+                            "job_index = 1\n"
+                            "\n"
+                            "\n";
+
+    FILE *fin = fmemopen(input, FILE_LEN, "r");
+    FILE *fout = fmemopen(output, FILE_LEN, "w");
+    job_array jobs = {.job_names = NULL, 0, 0};
+    read_jobs(fin, &jobs);
+    staff_array staff;
+    init_staff(&staff);
+    read_people(fin, &staff, jobs.size);
+    double time_old = 0;
+    double time_young = 0;
+    results young, old;
+    fclose(fin);
+    find_young(&young, &staff, jobs.size);
+    show_list_by_job(&young, fout);
+    free_result(&young);
+
+    find_old(&old, &staff, jobs.size);
+    show_list_by_job(&old, fout);
+    fclose(fout);
+    free_result(&old);
+
+    free_people(&staff);
+    free_jobs(&jobs);
+    EXPECT_STRCASEEQ(output, etalon);
 }
 
 
